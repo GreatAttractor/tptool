f363cc0a4380f778fc92820f55eca9985f6c14a1
Filip Szczerek <ga.software@yahoo.com>
2024-01-08 20:29:48 +0100

    Add async-std-based timer

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,10 @@
+use core::future::Future;
 use crate::key_poller::KeyPoller;
-use std::rc::Rc;
+use std::{pin::Pin, rc::Rc};
 
 pub struct ProgramState {
+    pub counter: usize,
     pub window: Rc<pancurses::Window>,
     pub key_poller: KeyPoller,
+    pub timer: Pin<Box<dyn Future<Output = ()>>>
 }
--- src/event_handling.rs
+++ src/event_handling.rs
@@ -4,18 +4,24 @@ use std::rc::Rc;
 
 pub async fn event_loop(window: Rc<pancurses::Window>) {
     let mut state = ProgramState{
+        counter: 0,
         window: Rc::clone(&window),
         key_poller: KeyPoller::new(window),
+        timer: Box::pin(pasts::Past::new((), |()| async_std::task::sleep(std::time::Duration::from_secs(1)))),
     };
 
-    let player_id = pasts::Loop::new(&mut state)
+    let _ = pasts::Loop::new(&mut state)
         .when(|s| &mut s.key_poller, on_key)
+        .when(|s| &mut s.timer, on_timer)
         .await;
 
     pancurses::endwin();
 }
 
-fn on_timer(state: &mut ProgramState, tick: usize) -> std::task::Poll<()> {
+fn on_timer(state: &mut ProgramState, _: ()) -> std::task::Poll<()> {
+    print_at(&state.window, 6, 0, &format!("tick: {}", state.counter), true);
+    state.counter += 1;
+
     std::task::Poll::Pending
 }
 
